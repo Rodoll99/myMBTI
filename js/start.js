70811b843d3e6cf72aaf93ad9e4341af99ffe993
Rodoll99 <dudtjr7137@naver.com>
2024-12-19 16:24:13 +0900

    js 및 결과 페이지 이미지 삽입

--- js/start.js
+++ js/start.js
@@ -4,47 +4,27 @@ const result = document.querySelector("#result");
 const endPoint = 12;
 
 //어떤 결과를 가져올지를 정하는 배열
-const select = [];
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 //배열에 따른 결과 (어떤 결과를 가져올지 배열 내의 요소 비율에따라 띄우기)
 function calResult() {
-  var pointArray = [
-    { name: "mouse", value: 0, key: 0 },
-    { name: "cow", value: 0, key: 1 },
-    { name: "tiger", value: 0, key: 2 },
-    { name: "rabbit", value: 0, key: 3 },
-    { name: "dragon", value: 0, key: 4 },
-    { name: "snake", value: 0, key: 5 },
-    { name: "horse", value: 0, key: 6 },
-    { name: "sheep", value: 0, key: 7 },
-    { name: "monkey", value: 0, key: 8 },
-    { name: "chick", value: 0, key: 9 },
-    { name: "dog", value: 0, key: 10 },
-    { name: "pig", value: 0, key: 11 },
-  ];
-  for (let i = 0; i < endPoint; i++) {
-    var target = qnaList[i].a[select[i]];
-    //질문 내의 type에 담겨있는 동물의 배열을 순환
-    for (let j = 0; j < target.type.length; j++) {
-      for (let k = 0; k < pointArray.length; k++) {
-        if (target.type[j] === pointArray[k].name) {
-          pointArray[k].value += 1;
-        }
-      }
-    }
-  }
-  //정렬 (밸류값을 이용)
-  var resultArray = pointArray.sort(function (a, b) {
-    if (a.value > b.value) {
-      return -1;
-    }
-    if (a.value < b.value) {
-      return 1;
-    }
-    return 0;
-  });
-  console.log(resultArray)
-  let resultword = resultArray[0].key;
-  return resultword;
+  var result = select.indexOf(Math.max(...select));
+  return result;
+}
+function setResult() {
+  let point = calResult();
+  const resultName = document.querySelector(".resultName");
+  resultName.innerHTML = infoList[point].name;
+
+  var resultImg = document.createElement("img");
+  const imgDiv = document.querySelector("#resultImg");
+  var imgURL = "img/image-" + point + ".png";
+  resultImg.src = imgURL;
+  resultImg.alt = point;
+  resultImg.classList.add("img-fluid");
+  imgDiv.appendChild(resultImg);
+
+  const resultDesc = document.querySelector(".resultDesc");
+  resultDesc.innerHTML = infoList[point].desc;
 }
 
 function goResult() {
@@ -59,7 +39,7 @@ function goResult() {
     }, 450); // 두 번째 setTimeout 닫음
   }); // 첫 번째 setTimeout 닫음
   console.log(select);
-  calResult();
+  setResult();
 } // goResult 함수 닫음
 
 function addAnswer(answerText, qIdx, idx) {
@@ -84,7 +64,11 @@ function addAnswer(answerText, qIdx, idx) {
         children[i].style.animation = "fadeOut 0.5s";
       }
       setTimeout(() => {
-        select[qIdx] = idx;
+        var target = qnaList[qIdx].a[idx].type;
+        for (let j = 0; j < target.length; j++) {
+          select[target[j]] += 1;
+        }
+
         for (let i = 0; i < children.length; i++) {
           children[i].style.display = "none";
         }
